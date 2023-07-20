5dcdae384a0eecc2de7debdb927b5faf7a27131b
Mond <herecomesthemoon@protonmail.com>
2023-07-20 23:57:15 +0200

    Cleaned up a bit. More work required

--- reminders.py
+++ reminders.py
@@ -3,37 +3,11 @@ from tabulate import tabulate
 from discord.ext import tasks, commands
 import asyncio
 import aiosqlite
-from parsedatetime import Calendar
-import datetime
 import botlog as bl
 import timeywimey
 from timeywimey import epoch2iso
 
 from config import IDGI
-
-"""
-What needs to be stored in the reminders database?
-int: running number, aka "reminder number X" 
-int: postID
-int: ID of person who made the request
-TEXT: status, Was the reminder executed? Is it in active memory? 
-TEXT: What they want to be reminded of
-TEXT: When they made the request
-TEXT: When it is due
-
-??? STATUS for tasks:
-Waiting ~ At some point far in the future
-Active/Threaded ~ Has an async sleep task associated to it, and fires soon (eg less than an hour) 
-Done ~ Completed. Someone was successfully reminded.
-Error ~ Something went wrong, eg. it was not possible to remind them.
-
-
-???
--STORE DATES IN EPOCH TIME: aka seconds after '1970-01-01T00:00:00'. Allows treating as ints, and ordering in sql
-
-Additional feature ideas:
-If the bot is restarted, ensure it doesn't drop any active reminder tasks. (EDIT: Probably done.)
-"""
 
 
 class Reminders(commands.Cog):
@@ -45,23 +19,13 @@ class Reminders(commands.Cog):
         )
         self.db = db
 
-    @tasks.loop(hours=1)
+    @tasks.loop(seconds=1)
     async def reminder_loop(self):
         await self.queue_reminders()
 
     @reminder_loop.before_loop
     async def before_reminder_loop(self):
         await self.bot.wait_until_ready()
-        p = await self.read_data(
-            """
-            WHERE queryDue <= (?) 
-            AND status IN ('Present', 'Future')
-            """,
-            [int(datetime.datetime.now().timestamp()) + 3600 + 60],
-        )
-        task_stack = [asyncio.create_task(self.notify(x)) for x in p]
-        if task_stack:
-            await asyncio.wait(task_stack)
 
     async def queue_reminders(self):
         p = await self.read_data(
@@ -69,57 +33,24 @@ class Reminders(commands.Cog):
             WHERE queryDue <= (?) 
             AND status LIKE 'Future'
             """,
-            [int(datetime.datetime.now().timestamp()) + 3600 + 60],
+            [timeywimey.right_now() + 1],
         )
         task_stack = [asyncio.create_task(self.notify(x)) for x in p]
         if task_stack:
             await asyncio.wait(task_stack)
 
     async def notify(self, p: aiosqlite.Row):
-        cur = await self.db.cursor()
         user = self.bot.get_user(p["userID"])
-        delay = p["queryDue"] - int(datetime.datetime.now().timestamp())
-        await cur.execute(
-            """
-            UPDATE memories 
-            SET status = 'Present' 
-            WHERE oid = (?)
-            """,
-            [p["rowid"]],
-        )
-        await self.db.commit()
-
-        await asyncio.sleep(max(delay, 1))
-
         try:
             await user.send(
                 content=f"{p['reminder']}.\n\nYou set a reminder on the <t:{p['queryMade']}>. "
                 f"Link to original query: {p['postUrl']}",
                 allowed_mentions=self.ping_priv,
             )
-            cur = await self.db.cursor()
-            await cur.execute(
-                """
-                UPDATE memories 
-                SET status = 'Past' 
-                WHERE oid = (?)
-                """,
-                [p["rowid"]],
-            )
-            await self.db.commit()
             bl.notification_triggered(tuple(p))
-        except discord.HTTPException:
-            bl.error_log.exception("Reminder notification error! Not good!")
-            owner = self.bot.get_user(self.bot.owner_id)
-            assert owner is not None
-            await owner.send(content="Reminder notification error! Not good!")
-            await owner.send(
-                content=f"{p['reminder']}.\n\nYou set a reminder on the <t:{p['queryMade']}>."
-                f"Link to original query: {p['postUrl']}",
-                allowed_mentions=self.ping_priv,
-            )
-            cur = await self.db.cursor()
-            await cur.execute(
+        except discord.DiscordException as e:
+            bl.error_log.exception(e)
+            await self.db.execute(
                 """
                 UPDATE memories 
                 SET status = 'Error' 
@@ -127,28 +58,32 @@ class Reminders(commands.Cog):
                 """,
                 [p["rowid"]],
             )
-            await self.db.commit()
+        else:
+            await self.db.execute(
+                """
+                UPDATE memories 
+                SET status = 'Past' 
+                WHERE oid = (?)
+                """,
+                [p["rowid"]],
+            )
+        await self.db.commit()
 
     @commands.command()
-    async def remindme(self, ctx, *, arg: str = ""):
+    async def remindme(self, ctx: commands.Context, *, arg: str = ""):
         """eg. !remindme 5 hours, take out the trash"""
         bl.log(self.remindme, ctx)
-        if "," in arg:  # Format argument, decide if 'what to be reminded of' was given.
+        if "," in arg:
             when, what = arg.split(sep=",", maxsplit=1)
-            when, what = when.strip(), what.strip()  # _, reminder
+            when, what = when.strip(), what.strip()
         else:
             when = arg.strip()
             what = when
-        cal = Calendar()
-        time_struct, parse_status = cal.parse(when)
-        t = datetime.datetime(*time_struct[:6])
-        now = int(datetime.datetime.now().timestamp())  # queryMade
-        try:
-            then = int(t.timestamp())  # queryThen
-        except OSError:
-            then = now
+        now, then, parse_status = timeywimey.parse_time(when)
+        if not parse_status:
+            await ctx.message.add_reaction(IDGI)
+            return
         p = {
-            # 0 ~ 'oid': INTEGER, primary key, increments automatically
             "userID": ctx.author.id,
             "postID": ctx.message.id,
             "postUrl": ctx.message.jump_url,
@@ -157,17 +92,11 @@ class Reminders(commands.Cog):
             "queryDue": then,
             "status": "Future",
         }
-        if p["queryDue"] <= p["queryMade"] + 5 or not parse_status:
-            await ctx.message.add_reaction(IDGI)
-            return
-        await ctx.message.reply(
-            content=f"You'll be reminded on the <t:{p['queryDue']}> of {p['reminder']}. "
-            f"Use ``!reminders`` to check your reminders.",
-            allowed_mentions=self.ping_priv,
-        )
-        p["oid"] = await self.add_data(
-            p
-        )  # Store new reminder in database, return primary key row_id
+        content = f"You'll be reminded on the <t:{p['queryDue']}> of {p['reminder']}. Use ``!reminders`` to check your reminders."
+        if "," not in arg:
+            content += " Please remember to use a `,` to separate *what* you want to be reminded from *when* you want to be reminded of it. Good: `!remindme 3 days, mom's birthday 2 days`. Bad: `!remindme 3 days mom's birthday 2 days`."
+        await ctx.message.reply(content, allowed_mentions=self.ping_priv)
+        p["oid"] = await self.add_data(p)
         await self.queue_reminders()
 
     @commands.command()
